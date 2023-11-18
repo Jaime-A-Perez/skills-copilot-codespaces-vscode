c6b032c2c2e349d5ce318068711baa0528d9fbd4
Jaime A Perez <82597268+Jaime-A-Perez@users.noreply.github.com>
2023-11-18 00:05:17 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,16 @@
+
+const http = require('http');
+
+// Create a server object
+const server = http.createServer((req, res) => {
+  // Set the response header
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  // Send a response to the client
+  res.end('Hello, world!');
+});
+
+// Start the server and listen on port 3000
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
